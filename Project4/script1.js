29243a1737b5c52360033d0462a4542dc769e788
mvevans <mvevans1@gmail.com>
2014-11-12 22:31:19 -0500

    done

--- Project4/script1.js
+++ Project4/script1.js
@@ -1,1 +1,36 @@
-//mark evans
+//mark evans project 4
+
+$( go );
+
+function go( )
+{
+ $( "#txtID" ).datepicker({ onSelect: dateAlert });
+ 
+ alert("Changing class of h1_2 and h1_3 to greenClass, new background color is green");
+ $(".h1Class").addClass("greenClass");
+ $(".greenClass").removeClass("h1Class");
+ 
+ alert("changing width of all paragraphs to 200");
+ $("p").width("200px");
+ 
+ alert("sliding second paragraph up");
+ $( "p:eq(1)" ).slideUp(400, slideUpDone);
+}
+
+function slideUpDone(){
+	alert("sliding second paragraph down");
+	$( "p:eq(1)" ).slideDown(2000, functionCall);
+}
+
+function functionCall(){
+	this.innerHTML = "the function was called";
+	
+	alert("making the paragraph with the id pID draggable and resizeable");
+	$("#pID").draggable();
+	$("#pID").resizable( { ghost: true, handles: 'ne, se, e'} );
+}
+
+function dateAlert( dateText, inst )
+{
+  alert( "Inside dateAlert, dateText is " + dateText );
+}
